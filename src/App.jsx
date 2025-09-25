6b0e0eb7d56098668e5ba7960e5a0c632e2e4e05
zzungwoo <jw4178@naver.com>
2025-09-25 17:42:39 +0900

    💄Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const stocks = [
 
 function App() {
   return (
-    <div>
-      <h1>오늘의 주식 시세</h1>
-      <div>
+    <div className='bg-gray-400 min-h-screen p-8 flex flex-col items-center'>
+      <h1 className="text-4xl font-bold text-gray-800 mb-10">오늘의 주식 시세</h1>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl" >
         {
           stocks.map(stock => (
             <TickerCard 
--- src/TickerCard.jsx
+++ src/TickerCard.jsx
@@ -5,22 +5,22 @@ const TickerCard = ({ ticker, name, currentPrice, previousClose }) => {
   const isPositive = priceChange >= 0
 
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
-        <div>{ticker}</div>
+    <div className="bg-white rounded-lg shadow-xl p-6 w-80 transform transition duration-500 hover:scale-110">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold text-gray-800">{name}</h2>
+        <div className="text-sm font-semibold text-gray-500">{ticker}</div>
       </div>
       <div></div>
       
-      <div>
+      <div className={`text-4xl font-extrabold mb-2 ${isPositive ? 'text-green-600' : 'text-red-600'}`}>
         ${currentPrice.toFixed(2)}
       </div>
 
-      <div>
+      <div className={`text-base font-semibold ${isPositive ? 'text-green-700' : 'text-red-900'}`}>
         {isPositive ? '▲' : '▼'} {priceChange.toFixed(2)}
       </div>
       
-      <div>
+      <div className="mt-4 text-sm text-gray-700">
         전일 종가: ${previousClose.toFixed(2)}
       </div>
     </div>
